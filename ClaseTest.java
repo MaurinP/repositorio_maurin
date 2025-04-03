e91657edb6725affb0fbaaf6a19b41c8ec6bca21
MaurinP <epadillap@unal.edu.co>
2025-04-03 15:52:40 -0500

    Add files via upload

---
+++ ClaseTest.java
@@ -0,0 +1,16 @@
+//comentario al azar para java
+
+public class ClaseTest {
+
+       public static void main(String[] args) {
+               //System.out.println("hola, mundo");
+               System.out.println("Ejercicio 1");
+               System.out.println("Hola, ya se imprimir frases\n");
+               System.out.println("Ejercicio 2");
+               int a = 37;
+               System.out.println(a+"\n");
+               System.out.println("Ejercicio 3");
+               double b = 1.5;
+               System.out.println(b+"\n");
+       }
+}
